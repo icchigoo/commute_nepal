5145408c12aa5bcc547d8903c08061cc8ea7015e
icchigoo <icchigoo12@gmail.com>
2023-02-20 09:19:13 -0800

    dsfbjh

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 
 import React from 'react';
 import {StatusBar} from 'react-native';
+import DestinationSearch from './src/screens/DestinationSearch';
 
 
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
+DestinationSearch
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <DestinationSearch/>
+
     </>
   );
 };
--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,29 +1,58 @@
-import React from "react";
-import { View, TextInput, SafeAreaView } from "react-native/types";
 
-import styles from "./styles";
-
-import React from "react";
-import { View, Dimensions, Text } from "react-native";
-import styles from "../../components/CovidMessage/styles";
+import React, {useState, useEffect} from 'react';
+import {View, TextInput, SafeAreaView} from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 
+import styles from './styles.js';
 
-const destinationSearch = (props) => {
-  
+const DestinationSearch = (props) => {
+  const [originPlace, setOriginPlace] = useState(null);
+  const [destinationPlace, setDestinationPlace] = useState(null);
 
-
+  useEffect(() => {
+    console.warn('useEffect is called');
+    if (originPlace && destinationPlace) {
+      console.warn('Redirect to results');
+    }
+  }, [originPlace, destinationPlace]);
 
   return (
     <SafeAreaView>
-       <View>
-      <TextInput style ={styles.TextInput} placeholder = "from" />
-      <TextInput style ={styles.TextInput} placeholder = "from" />
-       </View>
+      <View style={styles.container}>
 
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          onPress={(data, details = null) => {
+            setOriginPlace({data, details});
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails
+          query={{
+            key: 'AIzaSyBbIAS8BW3_tDo9u2RHA0TV8lIgD2eCse4',
+            language: 'en',
+          }}
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where to?"
+          onPress={(data, details = null) => {
+            setDestinationPlace({data, details});
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails
+          query={{
+            key: 'AIzaSyBbIAS8BW3_tDo9u2RHA0TV8lIgD2eCse4',
+            language: 'en',
+          }}
+        />
+      </View>
     </SafeAreaView>
-   
   );
 };
 
-export default destinationSearch;
+export default DestinationSearch;
